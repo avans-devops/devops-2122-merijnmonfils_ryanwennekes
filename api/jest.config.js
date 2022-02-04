6470f63fcc5101dc936d5fb47ead573033d4f89a
Ryan Wennekes <ryanwennekes@gmail.com>
2022-02-04 22:26:47 +0100

    fix: code styling

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,15 +1,15 @@
 const config = {
   verbose: true,
-  "testEnvironment": "node",
-    "coveragePathIgnorePatterns": [
-      "/node_modules/"
-    ],
-    "preset": "@shelf/jest-mongodb",
-    "coverageThreshold": {
-      "global": {
-        "lines": 60
-      }
+  'testEnvironment': 'node',
+  'coveragePathIgnorePatterns': [
+    '/node_modules/'
+  ],
+  'preset': '@shelf/jest-mongodb',
+  'coverageThreshold': {
+    'global': {
+      'lines': 60
     }
+  }
 };
 
 module.exports = config;
