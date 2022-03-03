2933c4e4e13bd94b3b2cd4ee21bc06f8a7412a72
Ryan Wennekes <rb.wennekes@student.avans.nl>
2022-03-03 13:50:26 +0100

    chore: test auto deploy

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'API App' });
+  res.render('index', { title: 'API Applicazione' });
 });
 
 module.exports = router;
