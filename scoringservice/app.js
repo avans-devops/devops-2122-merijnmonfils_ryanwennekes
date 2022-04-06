2d881496aac4a9bce94c02285a2d138defb04244
Ryan Wennekes <rb.wennekes@student.avans.nl>
2022-04-06 14:19:15 +0200

    feat: initialized microservices

--- scoringservice/app.js
+++ scoringservice/app.js
@@ -4,17 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const promBundle = require('express-prom-bundle');
-const metricsMiddleware = promBundle({
-  includePath: true,
-  includeStatusCode: true,
-  normalizePath: true,
-  promClient: {
-    collectDefaultMetrics: {}
-  }
-});
-
-var targetsRoute = require('./routes/targets');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -28,17 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(metricsMiddleware);
-
-app.use('/targets', targetsRoute);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
